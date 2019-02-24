7c94fad81f99f3046993a3ec8ba05e61cb986f00
linnnruoo <linruo2014@gmail.com>
2019-02-24 21:13:47 +0800

    remove unnecessary imports

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Home from './pages/Home'
-
 import NavBar from './components/NavBar';
-import Project from './pages/Project';
-import Workshop from './pages/Workshop';
-import Talk from './pages/Talk';
+
 import Committee from './pages/Committee';
+import Home from './pages/Home'
+import Project from './pages/Project';
+import Talk from './pages/Talk';
+import Workshop from './pages/Workshop';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
+        <div>
           <NavBar />
           <Route exact path="/" component={Home} />
           <Route exact path="/workshop" component={Workshop} />
